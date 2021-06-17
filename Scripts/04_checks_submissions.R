84d0c8241b3326a68281ebe831551a1160f20ac9
Baboyma Kagniniwa <baboyma@gmail.com>
2021-06-17 14:21:22 -0400

    Quick check for 06/15 Data

--- Scripts/04_checks_submissions.R
+++ Scripts/04_checks_submissions.R
@@ -23,7 +23,7 @@
     bkt_name <- "gov-usaid"
     
   # Date 
-    pdate <- '2021-05-28'
+    pdate <- '2021-06-15'
     
   # Run 2021-05-10
     dir_raws <- paste0("./Data/ddc-run-", pdate)
@@ -90,15 +90,15 @@
                       usecase = "hfr",
                       prefix = NULL) {
     
+    # Build object path
+    path <- glue::glue("{system}/{sys_env}/{dt_type}/{usecase}/{dt_route}/") %>% 
+      stringr::str_to_lower(.)
+    
+    # Check path exists
+    #message(glue("Checking main path: {path}"))
+    
     key <- tryCatch({
-      
-      # Build object path
-      path <- glue::glue("{system}/{sys_env}/{dt_type}/{usecase}/{dt_route}/") %>% 
-        stringr::str_to_lower(.)
-      
-      # Check path exists
-      #message("Checking main path ...")
-      
+      # check if path exists
       exists <- aws.s3::object_exists(object = path, 
                                       bucket = "gov-usaid",
                                       key = glamr::get_s3key("access"),
@@ -146,7 +146,8 @@
   #' @return Latest object key
   #' 
   s3_latest_outputs <- function(output = "errors", 
-                                df_outs = NULL) {
+                                df_outs = NULL,
+                                proc_date = NULL) {
     
     # Validate outputs
     outputs <- c("submissions", "mechanisms", "errors", "tableau")
@@ -175,8 +176,15 @@
     }
     
     # Detailed Errors
+    df_outs <- df_outs %>% 
+      filter(stringr::str_detect(stringr::str_to_lower(sys_data_object), pattern = key_pattern)) 
+    
+    if (!is.null(proc_date)) {
+      df_outs <- df_outs %>% 
+        filter(lubridate::ymd(base::as.Date(last_modified)) == proc_date)
+    }
+    
     key <- df_outs %>% 
-      filter(stringr::str_detect(stringr::str_to_lower(sys_data_object), pattern = key_pattern)) %>% 
       filter(last_modified == base::max(last_modified)) %>% 
       pull(key)
     
@@ -220,7 +228,7 @@
       )
     }
       
-    # Transform etadata
+    # Transform metadata
     df_meta <- df_meta %>% 
       mutate(filename = basename(file_subm),
              var = if_else(var == "Operating Unit", 
@@ -248,10 +256,12 @@
           return(tibble(
             filename = basename(file_subm), 
             sheet = s,
+            records = NA,
             rows = NA,
             rows_empty = NA,
             cols = NA,
-            cols_empty = NA
+            cols_empty = NA,
+            note = "Non-HFR Sheet"
           ))
         }
         
@@ -262,6 +272,20 @@
           skip = 1,
           col_types = "text"
         ) 
+        
+        # Check for valid columns
+        if (!all(Wavelength::template_cols_meta[1:7] %in% names(df))) {
+          return(tibble(
+            filename = basename(file_subm), 
+            sheet = s,
+            records = NA,
+            rows = NA,
+            rows_empty = NA,
+            cols = NA,
+            cols_empty = NA,
+            note = "Missing valid HFR Meta columns"
+          ))
+        }
         
         # Count empty rows
         r_empty <- df %>% 
@@ -274,14 +298,21 @@
           str_subset("\\d$") %>% 
           length()
         
+        # Count number of record
+        records <- df %>% 
+          Wavelength::hfr_gather(df = .) %>% 
+          nrow()
+          
         # Build output
         df <- df %>% 
           mutate(filename = basename(file_subm), 
                  sheet = s,
+                 records = records,
                  rows = nrow(df),
                  rows_empty = r_empty,
                  cols = ncol(df),
-                 cols_empty = c_empty) %>% 
+                 cols_empty = c_empty,
+                 note = NA) %>% 
           select(filename:cols_empty) %>% 
           distinct()
       })
@@ -305,20 +336,25 @@
       ) %>% 
       filter(str_detect(str_to_lower(sys_data_object), ".xlsx$")) %>% 
       mutate(last_modified = ymd(as.Date(last_modified))) %>% 
-      filter(last_modified == max(last_modified))
-    
-    df_raws %>% glimpse()
+      filter(
+        #last_modified == max(last_modified)
+        last_modified == pdate
+        )
   
   # Processed files
     df_procs <- s3_objects(
         bucket = bkt_name,
-        prefix = s3_path("processed", "incoming"),
+        #prefix = s3_path("processed", "incoming"),
+        prefix = "ddc/uat/processed/hfr/incoming/",
         n = Inf,
         unpack_keys = TRUE
       ) %>% 
       filter(str_detect(str_to_lower(sys_data_object), ".csv$")) %>% 
       mutate(last_modified = ymd(as.Date(last_modified))) %>% 
-      filter(last_modified == max(last_modified))
+      filter(
+        #last_modified == max(last_modified)
+        last_modified == pdate
+        )
     
   # Outputs
     df_outs <- s3_objects(
@@ -331,19 +367,19 @@
     
   # Detailed Errors
     key_errors <- df_outs %>% 
-      s3_latest_outputs(output = "errors", df_outs = .)
+      s3_latest_outputs(output = "errors", df_outs = ., proc_date = pdate)
     
   # Submission Status
     key_subms <- df_outs %>% 
-      s3_latest_outputs(output = "submissions", df_outs = .)
+      s3_latest_outputs(output = "submissions", df_outs = ., proc_date = pdate)
     
   # Mechanism Status
     key_mechs <- df_outs %>% 
-      s3_latest_outputs(output = "mechanisms", df_outs = .)
+      s3_latest_outputs(output = "mechanisms", df_outs = ., proc_date = pdate)
     
   # Tableau Output
     key_tbw <- df_outs %>% 
-      s3_latest_outputs(output = "tableau", df_outs = .)
+      s3_latest_outputs(output = "tableau", df_outs = ., proc_date = pdate)
   
     
 # DOWNLOAD ----
@@ -367,6 +403,11 @@
                        object = .,
                        filepath = file.path(dir_outs, basename(.))))
     
+    key_errors %>% 
+      map(~s3_download(bucket = bkt_name, 
+                       object = .,
+                       filepath = file.path(dir_outs, basename(.))))
+    
   # outputs (tableau)
     key_tbw %>% 
       s3_download(bucket = bkt_name, 
@@ -382,21 +423,20 @@
     
     meta_sum <- meta %>% 
       group_by(filename, operating_unit_country, hfr_fy_and_period, template_type) %>% 
-      summarise_at(all_of(c("rows", "rows_empty", "cols", "cols_empty")), 
+      summarise_at(all_of(c("records", "rows", "rows_empty", "cols", "cols_empty")), 
                    sum, na.rm = TRUE) %>% 
       ungroup()
       
-    
     meta_only <- dir_raws %>% 
       list.files(pattern = ".xlsx$", full.names = TRUE) %>% 
       map_dfr(hfr_metadata, add_structure = F)
   
-  # No Ou or Period period
+  # Subms with no Ou or Period period
     meta %>% 
       filter(is.na(operating_unit_country) | is.na(hfr_fy_and_period)) %>% 
       prinf()
   
-  # Empty Rows
+  # Subms with empty Rows
     meta %>% 
       filter(!is.na(operating_unit_country),
              !is.na(hfr_fy_and_period),
@@ -404,12 +444,12 @@
              rows_empty > 0) %>% 
       prinf()
   
-  # Empty Columns
+  # Subms with empty Columns
     meta %>% 
       filter(!is.na(operating_unit_country),
              !is.na(hfr_fy_and_period),
-             rows > 0,
-             rows_empty > 0) %>% 
+             cols > 0,
+             cols_empty > 0) %>% 
       prinf()
   
   # Subms Status vs Meta
@@ -418,17 +458,15 @@
         basename(key_subms), 
         full.names = TRUE
       ) %>%
-      vroom() 
-    
-    df_status %>% glimpse()
+      vroom() %>% 
+      filter(processed_date == pdate)
     
   # Check Submission status for failed files
     df_status %>% 
       filter(str_detect(status, "Entire file failed"),
              processed_date == pdate) %>% 
       left_join(meta_only, by = c("file_name" = "filename")) %>% 
-      select(-c(status:records_not_processed_due_to_errors)) %>% 
-      view()
+      select(-c(status:records_not_processed_due_to_errors)) 
     
     df_status %>% 
       filter(str_detect(status, "Entire file failed")) %>% 
@@ -436,8 +474,7 @@
       select(file_name, records_not_processed_due_to_errors:last_col()) %>%
       rowwise() %>% 
       mutate(rows_check = records_not_processed_due_to_errors == rows) %>% 
-      ungroup() %>% 
-      view()
+      ungroup() 
     
     df_status %>% 
       filter(str_detect(status, "Processing completed with errors")) %>% 
@@ -446,7 +483,7 @@
       rowwise() %>% 
       mutate(rows_check = sum(records_successfully_processed, 
                               records_not_processed_due_to_errors,
-                              na.rm = TRUE) == rows) %>% 
+                              na.rm = TRUE) == records) %>% 
       ungroup() %>% 
       view()
     
@@ -456,9 +493,17 @@
       basename(key_errors), 
       full.names = TRUE) %>%
       vroom() %>% 
-      filter(processed_date %in% c(pdate, ymd(pdate) + 1))
+      #filter(processed_date %in% c(pdate, ymd(pdate) + 1))
+      filter(processed_date == pdate)
     
-    df_errors %>% glimpse()
+    files_errors <- df_status %>% 
+      filter(str_detect(status, "Processing completed with errors")) %>% 
+      pull(file_name)
+    
+    files_errors
+    
+    df_errors %>% 
+      filter(file_name %in% files_errors)
     
       
     
